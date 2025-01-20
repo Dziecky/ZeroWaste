36127cb8f4be1c0f143212520a51637f5adba7ca
Kacper Przemielewski <86023@student.pb.edu.pl>
2025-01-20 13:30:00 +0100

    Reads count

--- src/main/java/projekt/zespolowy/zero_waste/services/EducationalServices/Article/ArticleService.java
+++ src/main/java/projekt/zespolowy/zero_waste/services/EducationalServices/Article/ArticleService.java
@@ -2,7 +2,6 @@ package projekt.zespolowy.zero_waste.services.EducationalServices.Article;
 
 import org.springframework.data.domain.Page;
 import org.springframework.data.domain.Pageable;
-import org.springframework.data.web.PagedModel;
 import projekt.zespolowy.zero_waste.dto.ArticleDTO;
 import projekt.zespolowy.zero_waste.entity.EducationalEntities.Articles.Article;
 import projekt.zespolowy.zero_waste.entity.EducationalEntities.Articles.ArticleCategory;
@@ -22,8 +21,7 @@ public interface ArticleService {
     Page<Article> findArticles(ArticleCategory category, String title, String tagName, Pageable pageable);
     void toggleLikeArticle(Long id);
     int getLikes(Long id);
-    Page<ArticleDTO> findArticlesWithLikes(ArticleCategory category, String title, String tagName, Pageable pageable, User currentUser);
+    Page<ArticleDTO> findArticlesWithLikesAndReads(ArticleCategory category, String title, String tagName, Pageable pageable, User currentUser);
     void toggleReadArticle(Long id);
     int getReads(Long id);
-    Page<ArticleDTO> findArticlesWithReads(ArticleCategory category, String title, String tagName, Pageable pageable, User currentUser);
 }
