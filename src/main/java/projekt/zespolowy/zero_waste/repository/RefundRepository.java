c0f87a679c3b8f740bfb4939b31401f29f0a27ca
Dominik Zawadzki <86226@student.pb.edu.pl>
2025-03-15 17:46:33 +0100

    Add entity Refund and implement Refund form.

---
+++ src/main/java/projekt/zespolowy/zero_waste/repository/RefundRepository.java
@@ -0,0 +1,7 @@
+package projekt.zespolowy.zero_waste.repository;
+
+import org.springframework.data.jpa.repository.JpaRepository;
+import projekt.zespolowy.zero_waste.entity.Refund;
+
+public interface RefundRepository extends JpaRepository<Refund, Long> {
+}
