ca6d8c74ecb88c35fec8a7ed8903d64293f1b61a
Dominik Zawadzki <86226@student.pb.edu.pl>
2025-04-26 10:19:25 +0200

    Admin Refund Details & ability to change Refund Status

--- src/main/java/projekt/zespolowy/zero_waste/services/RefundService.java
+++ src/main/java/projekt/zespolowy/zero_waste/services/RefundService.java
@@ -1,13 +1,16 @@
 package projekt.zespolowy.zero_waste.services;
 
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.data.crossstore.ChangeSetPersister;
 import org.springframework.data.domain.Page;
 import org.springframework.data.domain.PageRequest;
 import org.springframework.data.domain.Pageable;
 import org.springframework.data.domain.Sort;
 import org.springframework.stereotype.Service;
+import projekt.zespolowy.zero_waste.controller.exception.RefundNotFoundException;
 import projekt.zespolowy.zero_waste.entity.Refund;
 import projekt.zespolowy.zero_waste.entity.User;
+import projekt.zespolowy.zero_waste.entity.enums.RefundStatus;
 import projekt.zespolowy.zero_waste.repository.OrderRepository;
 import projekt.zespolowy.zero_waste.repository.RefundRepository;
 
@@ -34,5 +37,15 @@ public class RefundService {
     public Page<Refund> getAllRefunds(int page, int size) {
         Pageable pageable = PageRequest.of(page, size, Sort.by(Sort.Direction.DESC, "requestDate"));
         return refundRepository.findAllWithOrderAndUser(pageable);
+    }
+
+    public Refund findRefundById(Long id) {
+        return refundRepository.findById(id).orElseThrow(() -> new RefundNotFoundException(id));
+    }
+
+    public void updateRefundStatus(Long refundId, RefundStatus newStatus) {
+        Refund refund = findRefundById(refundId);
+        refund.setStatus(newStatus);
+        refundRepository.save(refund);
     }
 }
