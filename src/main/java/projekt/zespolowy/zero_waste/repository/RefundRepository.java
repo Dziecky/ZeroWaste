8bda6d10702fb9f9dbc73705f71d453902b2e90d
Dominik Zawadzki <86226@student.pb.edu.pl>
2025-04-26 09:12:04 +0200

    Add all refunds paginated view & authorize it for ADMIN only

--- src/main/java/projekt/zespolowy/zero_waste/repository/RefundRepository.java
+++ src/main/java/projekt/zespolowy/zero_waste/repository/RefundRepository.java
@@ -3,6 +3,7 @@ package projekt.zespolowy.zero_waste.repository;
 import org.springframework.data.domain.Page;
 import org.springframework.data.domain.Pageable;
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.jpa.repository.Query;
 import projekt.zespolowy.zero_waste.entity.Refund;
 import projekt.zespolowy.zero_waste.entity.User;
 
@@ -10,4 +11,9 @@ public interface RefundRepository extends JpaRepository<Refund, Long> {
 
     Page<Refund> findAllByOrder_User(User user, Pageable pageable);
 
+    @Query(
+            value = "SELECT r FROM Refund r JOIN FETCH r.order o JOIN FETCH o.user",
+            countQuery = "SELECT count(r) FROM Refund r"
+    )
+    Page<Refund> findAllWithOrderAndUser(Pageable pageable);
 }
