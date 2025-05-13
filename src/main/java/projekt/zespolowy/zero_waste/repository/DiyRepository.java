055e3a08953d300dfb4c3a5cc47a3ff02266ba06
Kacper Przemielewski <86023@student.pb.edu.pl>
2025-05-13 19:55:45 +0200

    diy projects

---
+++ src/main/java/projekt/zespolowy/zero_waste/repository/DiyRepository.java
@@ -0,0 +1,8 @@
+package projekt.zespolowy.zero_waste.repository;
+
+import org.springframework.data.jpa.repository.JpaRepository;
+import projekt.zespolowy.zero_waste.entity.Diy;
+
+public interface DiyRepository extends JpaRepository<Diy, Long> {
+
+}
